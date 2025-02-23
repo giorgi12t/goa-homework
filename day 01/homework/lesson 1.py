df35fdb94b8e41398c35c54cdf743dc63969b325
giorgi12t <giogio77@mail.ru>
2025-02-23 19:00:47 +0400

    commit

---
+++ day 01/homework/lesson 1.py
@@ -0,0 +1,5 @@
+name="giorgi"
+lastname="tsurtsumia"
+age=12
+hobby="computer games"
+print(" i am " + name +" " + lastname + " " + str(age) + "y.o my hobby is " + hobby)
---
+++ day 02/class work/lesson 1.py
@@ -0,0 +1,7 @@
+#printის დახმარებით, ტერმინალში გამოიტანეთ ყველა ნასწავლი მონაცემთა ტიპი(Data type)
+s="tank"
+i=34
+f=34.5
+print(type(s))
+print(type(i))
+print(type(f))
