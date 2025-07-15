c45150429545f31654b2fd3f05b95036d447176c
giorgi12t <giogio77@mail.ru>
2025-07-15 08:55:35 +0400

    commit

---
+++ day 20/classswork/lesson20.py
@@ -0,0 +1,5 @@
+# მომხმარებლის შემოტანილი რიცხვი თუ ნაკლებია 15ზე, გამოიტანე რომ რიცხვი არის ნაკლები 15ზე და თუ მეტია, მაშინ გამოიტანე რომ რიცხვი არის 15ზე მეტი
+num1=int(input("enter number:15"))
+num2=int(input("enter enother number:3"))
+if num1>num2:
+    print("your firstnumber is bigger then second number")
