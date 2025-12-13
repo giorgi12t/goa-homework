4b9ed7c1b1af4faf98cdf39cf74a89eefb787381
giorgi12t <giogio77@mail.ru>
2025-12-13 15:47:10 +0400

    'commit'

---
+++ day 20/homework/lesson20.py
@@ -0,0 +1,12 @@
+#მომხმარებელს სთხოვე კვირის დღე კვირის თხოვნისთვის გამოიყენეც input და შესამოწმებლად(if/elif/else). 
+s = input("აირჩიეთ კვირის დღე : ")
+if s == "Monday":
+    print("good")
+
+v = input("აირჩიეთ კვირის დღე : ")
+if v == "Sunday":
+    print("sus")
+
+b = input("აირჩიეთ კვირის დღე : ")
+if b == "Saturday":
+    print("lol")
